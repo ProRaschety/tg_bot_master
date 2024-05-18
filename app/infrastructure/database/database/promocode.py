afbea0b4ef2abbfc1c694b4d5b7c51ea97bdf2df
Ravshan <pbtp_cfd@mail.ru>
2024-05-18 15:59:49 +0300

    add FDS tools

--- app/infrastructure/database/database/promocode.py
+++ app/infrastructure/database/database/promocode.py
@@ -21,91 +21,91 @@ class _PromocodeDB:
         self.connect = connect
 
     async def create_table_promocodes(self) -> None:
-        async with self.connect.transaction():
-            await self.connect.execute('''
-                CREATE TABLE IF NOT EXISTS promocodes(
-                    id SERIAL PRIMARY KEY,
-                    promocode VARCHAR(20) UNIQUE,
-                    created TIMESTAMPTZ DEFAULT NOW(),
-                    valid_until TIMESTAMPTZ DEFAULT NOW()
-                );
-            ''')
-            log.info("Created table '%s'", self.__table_name__)
+        # async with self.connect.transaction():
+        await self.connect.execute('''
+            CREATE TABLE IF NOT EXISTS promocodes(
+                id SERIAL PRIMARY KEY,
+                promocode VARCHAR(20) UNIQUE,
+                created TIMESTAMPTZ DEFAULT NOW(),
+                valid_until TIMESTAMPTZ DEFAULT NOW()
+            );
+        ''')
+        log.info("Created table '%s'", self.__table_name__)
 
     async def update_promocode_start(self) -> None:
-        async with self.connect.transaction():
-            for promocode in promocodes:
-                await self.connect.execute('''
-                    INSERT INTO promocodes(promocode, created, valid_until)
-                    VALUES($1, $2, $3) ON CONFLICT DO NOTHING;
-                ''', promocode['PROMO'], datetime.utcnow(), datetime.utcnow()
-                )
-                log.info(
-                    "Promocode update. db='%s', promocode=%s",
-                    self.__table_name__, promocode
-                )
+        # async with self.connect.transaction():
+        for promocode in promocodes:
+            await self.connect.execute('''
+                INSERT INTO promocodes(promocode, created, valid_until)
+                VALUES($1, $2, $3) ON CONFLICT DO NOTHING;
+            ''', promocode['PROMO'], datetime.utcnow(), datetime.utcnow()
+            )
+            log.info(
+                "Promocode update. db='%s', promocode=%s",
+                self.__table_name__, promocode
+            )
 
     async def add_promocode(
             self,
             *,
             promocode: str
     ) -> None:
-        async with self.connect.transaction():
-            await self.connect.execute('''
-                INSERT INTO promocodes(promocode, created, valid_until)
-                VALUES($1, $2, $3) ON CONFLICT DO NOTHING;
-            ''', promocode, datetime.utcnow(), datetime.utcnow()
-                                       )
-            log.info(
-                "Promocode added. db='%s', promocode=%s, date_time='%s'",
-                self.__table_name__, promocode, datetime.utcnow()
-            )
+        # async with self.connect.transaction():
+        await self.connect.execute('''
+            INSERT INTO promocodes(promocode, created, valid_until)
+            VALUES($1, $2, $3) ON CONFLICT DO NOTHING;
+        ''', promocode, datetime.utcnow(), datetime.utcnow()
+                                   )
+        log.info(
+            "Promocode added. db='%s', promocode=%s, date_time='%s'",
+            self.__table_name__, promocode, datetime.utcnow()
+        )
 
     async def get_promocode_user(self) -> PromocodeModel | None:
-        async with self.connect.transaction():
-            cursor = await self.connect.cursor('''
-                SELECT id,
-                    promocode,
-                    created,
-                    valid_until
-                FROM promocodes;
-            '''
-                                               )
-            data = await cursor.fetchrow()
-            return PromocodeModel(*data) if data else None
+        # async with self.connect.transaction():
+        cursor = await self.connect.cursor('''
+            SELECT id,
+                promocode,
+                created,
+                valid_until
+            FROM promocodes;
+        '''
+                                           )
+        data = await cursor.fetchrow()
+        return PromocodeModel(*data) if data else None
 
     async def get_promocode_list(self) -> PromocodeList | None:
-        async with self.connect.transaction():
-            cursor = await self.connect.cursor('''
-                SELECT *
-                FROM promocodes;
-            '''
-                                               )
-            data = await cursor.fetch(50)
-            # return PromocodeList(data) if data else None
-            return data
+        # async with self.connect.transaction():
+        cursor = await self.connect.cursor('''
+            SELECT *
+            FROM promocodes;
+        '''
+                                           )
+        data = await cursor.fetch(50)
+        # return PromocodeList(data) if data else None
+        return data
 
     async def get_valid_promocode_user(self, promocode: str) -> bool:
-        async with self.connect.transaction():
-            cursor = await self.connect.cursor('''
-                SELECT COUNT(DISTINCT promocode)
-                FROM promocodes
-                WHERE promocode = $1;
-            ''', promocode
-                                               )
-            data = await cursor.fetchrow()
-            # log.info(data)
-            return data
+        # async with self.connect.transaction():
+        cursor = await self.connect.cursor('''
+            SELECT COUNT(DISTINCT promocode)
+            FROM promocodes
+            WHERE promocode = $1;
+        ''', promocode
+                                           )
+        data = await cursor.fetchrow()
+        # log.info(data)
+        return data
 
     async def delete(self) -> None:
-        async with self.connect.transaction():
-            await self.connect.execute('''
-                    DELETE FROM promocodes;
-                '''
-                                       )
-            log.info(
-                "Promocodes clear. db='%s'", self.__table_name__
-            )
+        # async with self.connect.transaction():
+        await self.connect.execute('''
+                DELETE FROM promocodes;
+            '''
+                                   )
+        log.info(
+            "Promocodes clear. db='%s'", self.__table_name__
+        )
 
     # async def get_promocode_user(self, *, promocode: str) -> PromocodeModel | None:
     #     async with self.connect.transaction():
--- app/infrastructure/database/database/users.py
+++ app/infrastructure/database/database/users.py
@@ -16,21 +16,21 @@ class _UsersDB:
         self.connect = connect
 
     async def create_table(self) -> None:
-        async with self.connect.transaction():
-            await self.connect.execute('''
-                CREATE TABLE IF NOT EXISTS users(
-                    id SERIAL PRIMARY KEY,
-                    user_id BIGINT NOT NULL UNIQUE,
-                    created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-                    language VARCHAR(10),
-                    is_alive BOOLEAN NOT NULL,
-                    is_blocked BOOLEAN NOT NULL,
-                    role VARCHAR(20) NOT NULL,
-                    role_update TIMESTAMP NOT NULL DEFAULT NOW(),
-                    promocode VARCHAR(20)
-                );
-            ''')
-            logger.info("Created table '%s'", self.__table_name__)
+        # async with self.connect.transaction():
+        await self.connect.execute('''
+            CREATE TABLE IF NOT EXISTS users(
+                id SERIAL PRIMARY KEY,
+                user_id BIGINT NOT NULL UNIQUE,
+                created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                language VARCHAR(10),
+                is_alive BOOLEAN NOT NULL,
+                is_blocked BOOLEAN NOT NULL,
+                role VARCHAR(20) NOT NULL,
+                role_update TIMESTAMP NOT NULL DEFAULT NOW(),
+                promocode VARCHAR(20)
+            );
+        ''')
+        logger.info("Created table '%s'", self.__table_name__)
 
     async def add(
             self,
@@ -42,19 +42,19 @@ class _UsersDB:
             role: UserRole
     ) -> None:
 
-        async with self.connect.transaction():
-            await self.connect.execute('''
-                INSERT INTO users(user_id, language, is_alive, is_blocked, role, role_update)
-                VALUES($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING;
-            ''', user_id, language, is_alive, is_blocked, role, datetime.utcnow()
-                                       )
+        # async with self.connect.transaction():
+        await self.connect.execute('''
+            INSERT INTO users(user_id, language, is_alive, is_blocked, role, role_update)
+            VALUES($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING;
+        ''', user_id, language, is_alive, is_blocked, role, datetime.utcnow()
+                                   )
 
-            logger.info(
-                "User added. db='%s', user_id=%d, date_time='%s', "
-                "language='%s', is_alive=%s, is_blocked=%s, role=%s, date_time='%s'",
-                self.__table_name__, user_id, datetime.utcnow(), language,
-                is_alive, is_blocked, role, datetime.utcnow()
-            )
+        logger.info(
+            "User added. db='%s', user_id=%d, date_time='%s', "
+            "language='%s', is_alive=%s, is_blocked=%s, role=%s, date_time='%s'",
+            self.__table_name__, user_id, datetime.utcnow(), language,
+            is_alive, is_blocked, role, datetime.utcnow()
+        )
 
     async def update(
             self,
@@ -64,15 +64,15 @@ class _UsersDB:
             # role_update: datetime.utcnow()
     ) -> None:
         role_update = datetime.utcnow()
-        async with self.connect.transaction():
-            await self.connect.execute('''
-                UPDATE users SET role = $1, role_update=$2 WHERE user_id = $3;
-            ''', role, datetime.utcnow(), user_id
-                                       )
-            logger.info(
-                "User update. db='%s', role=%s, date_time='%s', user_id=%d",
-                self.__table_name__, role.value, datetime.utcnow(), user_id
-            )
+        # async with self.connect.transaction():
+        await self.connect.execute('''
+            UPDATE users SET role = $1, role_update=$2 WHERE user_id = $3;
+        ''', role, datetime.utcnow(), user_id
+                                   )
+        logger.info(
+            "User update. db='%s', role=%s, date_time='%s', user_id=%d",
+            self.__table_name__, role.value, datetime.utcnow(), user_id
+        )
 
     async def update_promocode(
             self,
@@ -80,41 +80,41 @@ class _UsersDB:
             user_id: int,
             promocode: str,
     ) -> None:
-        async with self.connect.transaction():
-            await self.connect.execute('''
-                UPDATE users SET promocode = $1 WHERE user_id = $2;
-            ''', promocode, user_id
-                                       )
-            logger.info(
-                "User update. db='%s', promocode=%s, date_time='%s', user_id=%d",
-                self.__table_name__, promocode, datetime.utcnow(), user_id
-            )
+        # async with self.connect.transaction():
+        await self.connect.execute('''
+            UPDATE users SET promocode = $1 WHERE user_id = $2;
+        ''', promocode, user_id
+                                   )
+        logger.info(
+            "User update. db='%s', promocode=%s, date_time='%s', user_id=%d",
+            self.__table_name__, promocode, datetime.utcnow(), user_id
+        )
 
     async def delete(self, *, user_id: int) -> None:
-        async with self.connect.transaction():
-            await self.connect.execute('''
-                    DELETE FROM users WHERE user_id = $1;
-                ''', user_id
-                                       )
-            logger.info(
-                "User deleted. db='%s', user_id='%d'", self.__table_name__, user_id
-            )
+        # async with self.connect.transaction():
+        await self.connect.execute('''
+                DELETE FROM users WHERE user_id = $1;
+            ''', user_id
+                                   )
+        logger.info(
+            "User deleted. db='%s', user_id='%d'", self.__table_name__, user_id
+        )
 
     async def get_user_record(self, *, user_id: int) -> UsersModel | None:
-        async with self.connect.transaction():
-            cursor = await self.connect.cursor('''
-                SELECT id,
-                    user_id,
-                    created,
-                    language,
-                    is_alive,
-                    is_blocked,
-                    role,
-                    role_update,
-                    promocode
-                FROM users
-                WHERE users.user_id = $1
-            ''', user_id
-                                               )
-            data = await cursor.fetchrow()
-            return UsersModel(**data) if data else None
+        # async with self.connect.transaction():
+        cursor = await self.connect.cursor('''
+            SELECT id,
+                user_id,
+                created,
+                language,
+                is_alive,
+                is_blocked,
+                role,
+                role_update,
+                promocode
+            FROM users
+            WHERE users.user_id = $1
+        ''', user_id
+                                           )
+        data = await cursor.fetchrow()
+        return UsersModel(**data) if data else None
